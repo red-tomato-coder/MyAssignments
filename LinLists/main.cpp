b672690a0f96ddd84c2994208bf8d0cf7e1e4aed
* <antonkozachenko212@gmail.com>
2024-10-29 15:42:30 +0200

    шото робив знову

--- LinLists/main.cpp
+++ LinLists/main.cpp
@@ -9,11 +9,20 @@ void menu(){
 	cout<<"MENU:\n"<<"1. PUSH\n"<<"2. POP\n"<<"3. SHOW\n"<<"Enter input: \n";
 	cin>>choice;
 	switch (choice)
-	case 1: cout<<"Enter a value to push: "; cin>>value; push(head, value); break;
-	case 2: pop(head); break;
-	case 3: show(head); break;
-	default: cout<<"Invalid choice"<<endl;
-	
+    case 1:
+      cout<<"Enter a value to push: "; 
+      cin>>value; 
+      push(head, value); 
+      break;
+	  case 2: 
+      pop(head); 
+      break;
+	  case 3: 
+      show(head); 
+      break;
+	  default: 
+      cout<<"Invalid choice"<<endl;
+  }
 }
 
 int main(){
